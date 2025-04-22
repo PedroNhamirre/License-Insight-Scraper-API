55b0793efe53646236d160e416b6d1fbb1c81d0b
PedroNhamirre <101422045+PedroNhamirre@users.noreply.github.com>
2025-04-22 12:11:39 +0200

    Update urls.py

--- inattro/inattro/urls.py
+++ inattro/inattro/urls.py
@@ -21,6 +21,11 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 schema_view = get_schema_view(
    openapi.Info(
