f60e7e3e5d25c195b6a61a0f652812d9b89da20b
PedroNhamirre <101422045+PedroNhamirre@users.noreply.github.com>
2025-04-22 12:18:59 +0200

    Update urls.py

--- inattro/inattro/urls.py
+++ inattro/inattro/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for inattro project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 
@@ -24,25 +7,35 @@ from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="License Insight API",
-      default_version='v1',
-      description="API documentation for the License Insight project",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@licenseinsight.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
-
+# Defina as URLs primeiro
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/', get_schema_view(
+        openapi.Info(
+            title="License Insight API",
+            default_version='v1',
+            description="API documentation for the License Insight project",
+            terms_of_service="https://www.google.com/policies/terms/",
+            contact=openapi.Contact(email="contact@licenseinsight.local"),
+            license=openapi.License(name="BSD License"),
+        ),
+        public=True,
+        permission_classes=(permissions.AllowAny,),
+    ).with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', get_schema_view(
+        openapi.Info(
+            title="License Insight API",
+            default_version='v1',
+            description="API documentation for the License Insight project",
+            terms_of_service="https://www.google.com/policies/terms/",
+            contact=openapi.Contact(email="contact@licenseinsight.local"),
+            license=openapi.License(name="BSD License"),
+        ),
+        public=True,
+        permission_classes=(permissions.AllowAny,),
+    ).with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+# Adicione a configuração para arquivos estáticos depois de definir urlpatterns
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
