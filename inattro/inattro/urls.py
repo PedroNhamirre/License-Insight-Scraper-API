d5771e3424e6dda46bb97e44c184c99295a6986f
PedroNhamirre <101422045+PedroNhamirre@users.noreply.github.com>
2025-04-22 12:37:51 +0200

    Update urls.py

--- inattro/inattro/urls.py
+++ inattro/inattro/urls.py
@@ -1,9 +1,14 @@
+from django.conf import settings  
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+
+
+
 
 # Defina as URLs primeiro
 urlpatterns = [
